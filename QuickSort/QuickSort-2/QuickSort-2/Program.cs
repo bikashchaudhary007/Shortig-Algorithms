ad071d1b5de82000c40a97d506abf9dbcee9813e
Bikash <bikashchaudhary007@gmail.com>
2022-12-09 11:57:27 +0545

    Quicksort and Quicksort-2

---
+++ QuickSort/QuickSort-2/QuickSort-2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace QuickSort_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] array = new int[] { 3, 2, 8, 4, 9 };
+            int[] array = { 51, 95, 66, 72, 42, 38, 62 };
+            int n = array.Length;
+            quickSort(array, 0, n-1);
+            Console.WriteLine("Unsorted List : " + String.Join(", ", array));
+            Console.WriteLine("Sorted List : " + String.Join(",", array));
+        }
+
+        public static int partition(int[] array, int low, int high)
+        {
+            int pivot = array[low];
+            int i = low+1;
+            int j = high;
+
+            do
+            {
+                while (array[i] <= pivot)
+                {
+                    i++;
+                }
+
+                while (array[j] > pivot)
+                {
+                    j--;
+                }
+
+                if (i < j)
+                {
+                    //swap
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+            while (i<j);
+
+            //Swap 
+            int tem = array[low];
+            array[low] = array[j];
+            array[j] = tem;
+            return j;
+        }
+
+        public static void quickSort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pindx = partition(array, low, high);
+                quickSort(array, low, pindx - 1);
+                quickSort(array, pindx + 1, high);
+            }
+
+        }
+    }
+}
---
+++ QuickSort/QuickSort/QuickSort/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] array = new int[] { 3, 2, 8, 4, 9 };
+            int[] array = { 51, 66, 72, 42, 38, 62};
+            int n = array.Length;
+            quickSort(array, 0, n - 1);
+            Console.WriteLine("Unsorted List : " + String.Join(", ", array));
+            Console.WriteLine("Sorted List : " + String.Join(",", array));
+        }
+
+        public static int partition(int[] array, int low, int high)
+        {
+            int pivot = array[high];
+            int i = low-1;
+
+            for (int j = low; j<high; j++)
+            {
+                if (array[j] < pivot)
+                {
+                    i++;
+                    //swap
+                    int tem = array[i];
+                    array[i] = array[j];
+                    array[j] = tem;
+                }
+               
+            }
+            //swap
+            i++;
+            int temp = array[i];
+            array[i] = pivot;
+            array[high] = temp;
+
+            return i; 
+        }
+
+        public static void quickSort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pindx = partition(array, low, high);
+                quickSort(array, low, pindx - 1);
+                quickSort(array, pindx + 1, high);
+            }
+
+        }
+
+
+    }
+}
