c6a9bdb9aa7d8490caef7d4f3ad192c98f257008
Bikash <bikashchaudhary007@gmail.com>
2022-12-11 12:53:14 +0545

    Insertion Sort

---
+++ InsertionSort/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {7, 8, 3, 2, 1};
+            Console.WriteLine("Unsorted List: " + String.Join(",",array));
+            insertionSort(array);
+            Console.WriteLine("Sorted List: " + String.Join(",", array));
+
+        }
+
+        public static void insertionSort(int[] array)
+        {
+            for (int i = 1; i<array.Length; i++)
+            {
+                int current = array[i];
+                int j = i - 1;
+                
+                while(j>=0 && current < array[j])
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+
+                //placement
+                array[j + 1] = current;
+            }
+        }
+    }
+}
